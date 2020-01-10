781d7a32757aef447f3542319d8d4d60886a83f6
Robert Mucha <r.mucha@npc.pl>
2020-01-10 10:51:22 +0100

    Add descriptions to classes,methods, properties and variables. Cleaned the code.

--- RSync/Areas/MainWindow/ViewModels/MainWindowVM.cs
+++ RSync/Areas/MainWindow/ViewModels/MainWindowVM.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RSync.Areas.MainWindow.ViewModels
 {
+    /// <summary>
+    /// View model from MainWindow
+    /// </summary>
     internal sealed class MainWindowVM
     {
+        /// <summary>
+        /// Property to display application title
+        /// </summary>
         public static string WindowTitle => AppDomain.CurrentDomain.FriendlyName;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public MainWindowVM()
         {
-
         }
     }
 }
